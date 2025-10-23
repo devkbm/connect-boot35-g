255173995ef3686f2810dad62fd6d02c3adf6df2
devkbm <kbm0417@nate.com>
2025-10-23 16:59:59 +0900

    2025-10-23

--- src/main/java/com/like/system/user/adapter/in/web/SystemUserSelectController.java
+++ src/main/java/com/like/system/user/adapter/in/web/SystemUserSelectController.java
@@ -5,6 +5,11 @@ import static com.like.core.web.util.ResponseEntityUtil.toOne;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +20,10 @@ import com.like.core.message.MessageUtil;
 import com.like.system.user.application.port.in.select.SystemUserSelectDTO;
 import com.like.system.user.application.port.in.select.SystemUserSelectUseCase;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @RestController
 public class SystemUserSelectController {		
 				
@@ -30,6 +38,22 @@ public class SystemUserSelectController {
 	public ResponseEntity<?> getUser(@RequestParam String companyCode, @PathVariable String userId) throws FileNotFoundException, IOException {
 												
 		SystemUserSelectDTO dto = useCase.selectDTO(userId, companyCode);					
+		
+		ExpressionParser parser = new SpelExpressionParser();
+		
+		EvaluationContext context = new StandardEvaluationContext(dto);
+		context.setVariable("num1", 10);
+        //context.setVariable("num2", 20);
+		
+		String rule = "userId == 'admin' && name == '관리자' && #num1 == 20 ";
+		
+		// 표현식 구문 분석
+		Expression expression = parser.parseExpression(rule);
+
+		// 평가 실행 및 결과 확인 (true 또는 false)
+		Boolean result = expression.getValue(context, Boolean.class); // true
+				
+		log.info(result.toString());
 		
 		return toOne(dto, MessageUtil.getQueryMessage(1));
 	}			
