430140a083a3f251f69740a6941b36837790bfa4
devkbm <kbm0417@nate.com>
2025-11-06 17:49:30 +0900

    2025-11-06

--- src/main/java/com/like/hrm/payitem/domain/payinfo/PayStaffInfo.java
+++ src/main/java/com/like/hrm/payitem/domain/payinfo/PayStaffInfo.java
@@ -7,9 +7,9 @@ import com.like.core.jpa.domain.AbstractAuditEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,15 +22,29 @@ import lombok.NoArgsConstructor;
 @Table(name = "HRMPAYSTAFF")
 @EntityListeners(AuditingEntityListener.class)
 public class PayStaffInfo extends AbstractAuditEntity {
-
-	@EmbeddedId
-	PayStaffInfoId id;
 	
-	@Embedded
-	PayStaffAppointment appointment;
+	// companyCode + payYm + seq + staffNo 
+	@Id
+	@Column(name="PAY_STAFF_ID")
+	String id;
+	
+	@Column(name="ORG_CD")
+	String companyCode;
 		
+	@Column(name="PAY_YYYYMM")
+	String payYm;
+	
+	@Column(name="SEQ")
+	Long seq;
+	
+	@Column(name="STAFF_NO")
+	String staffNo;
+	
 	@Comment("마감여부")
 	@Column(name="CLOSE_YN")
 	Boolean isClose;
 	
+	@Embedded
+	PayStaffAppointment appointment;
+			
 }
--- src/main/java/com/like/hrm/payitem/domain/payinfo/PayStaffItem.java
+++ src/main/java/com/like/hrm/payitem/domain/payinfo/PayStaffItem.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.like.core.jpa.domain.AbstractAuditEntity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,10 +23,15 @@ import lombok.NoArgsConstructor;
 @EntityListeners(AuditingEntityListener.class)
 public class PayStaffItem extends AbstractAuditEntity {
 
-	@EmbeddedId	
-	PayStaffItemId id;
+	@Id
+	@Column(name="PAY_STAFF_ID")
+	String id;
+	
+	@Column(name="PAY_ITEM_CODE")
+	String payItemCode;
 	
 	@Comment("금액")
 	@Column(name="WAGE_AMOUNT")
 	BigDecimal wageAmount;
+		
 }
